235706d529264b72774b96f07f145e671d388680
Arjun Divraniya <arjun.divraniya.cg@gmail.com>
2025-09-24 00:01:09 +0530

    Time: 4 ms (39.32%), Space: 9.6 MB (74.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,7 +3,7 @@
 //     bool isAnagram(string s, string t) {
 //         if (s.length() != t.length()) return false;
 
-//         vector<int> count(26, 0);
+//         vector<int> count(26, 0);   
 //         for (int i = 0; i < s.length(); i++) {
 //             count[s[i] - 'a']++;
 //             count[t[i] - 'a']--;
@@ -17,36 +17,15 @@
 //     }
 // };
 
-// class Solution {
-// public:
-//     bool isAnagram(string s, string t) {
-//         if (s.length() != t.length())
-//             return false;
-//         sort(s.begin(), s.end());
-//         sort(t.begin(), t.end());
-//         return s == t;
-//     }
-// };
 
-#include <unordered_set>
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length())return false;
-          
-        unordered_map<char, int> m;
-        for (char c : s) {
-            m[c]++;
-        }
-        for (char c : t) {
-            m[c]--;
-        }
-
-        for(auto&p : m){
-            if(p.second != 0){
-                return false;
-            }
-        }
-        return true;
+        if (s.length() != t.length()) return false;
+sort(s.begin(),s.end());
+sort(t.begin(),t.end());
+return s == t;
+       
     }
 };
+
