8c55c528e7677313c7d7015ef7ddc3275ce23023
Arjun Divraniya <arjun.divraniya.cg@gmail.com>
2025-10-07 10:33:27 +0530

    Time: 43 ms (16.84%), Space: 80.4 MB (20.08%) - LeetHub

--- 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
@@ -4,15 +4,18 @@ public:
         int count=0;
         sort(costs.begin(),costs.end());
         for(int i=0;i<costs.size();i++){
-            if(coins<costs[i]){
+            if(costs[i]>coins)break;
+            if(coins>0){
+                coins=coins-costs[i];
+            count++;
+                
+            }else{
                 break;
             }
-            coins=coins-costs[i];
-            count++;
-            if(coins==0){
-                return count;
+            
+           
             }
-        }
+        
         return count;
     }
 };
