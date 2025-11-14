d9a24e9e004a440071fde5c60b96f5b539f2bd3d
Arjun Divraniya <arjun.divraniya.cg@gmail.com>
2025-11-14 14:41:50 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,20 +3,25 @@ public:
     bool isValid(string s) {
         stack<char> st;
         for (char c : s) {
-            if (c == '(' || c == '[' || c == '{') {
+            if (c == '(' || c == '{' || c == '[') {
                 st.push(c);
             } else {
-                if (st.empty())
+                if (st.empty()) {
                     return false;
-                if (c == ')' && st.top() != '(')
+                }
+
+                if (c == ')' && st.top() != '(') {
                     return false;
-                if (c == ']' && st.top() != '[')
+                }
+                if (c == ']' && st.top() != '[') {
                     return false;
-                if (c == '}' && st.top() != '{')
+                }
+                if (c == '}' && st.top() != '{') {
                     return false;
+                }
                 st.pop();
             }
         }
-        return st.empty();
-    }
-};
+            return st.empty();
+        }
+    };
