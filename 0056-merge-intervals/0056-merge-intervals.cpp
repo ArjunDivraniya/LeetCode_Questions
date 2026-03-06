c589f2c44e8c345166415ad5c35faa3083c3a3f2
Arjun Divraniya <arjun.divraniya.cg@gmail.com>
2026-03-06 11:19:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        
+        sort(intervals.begin(),intervals.end());
+        int j =0;
+        for(int i=1;i<intervals.size();i++){
+            if(ans.empty()){
+                ans.push_back(intervals[0]);
+            }
+            if(intervals[i][0] <= ans[j][1]){
+                ans[j][1] =intervals[i][1];
+
+            }else{
+                ans.push_back(intervals[i]);
+                j++;
+            }
+        }
+return ans ;
+    }
+};
