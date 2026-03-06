7adaef9d068ec58094eb17a66c29b5b374861d5f
Arjun Divraniya <arjun.divraniya.cg@gmail.com>
2026-03-06 14:48:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,22 +1,8 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
+        unordered_set<int> s(nums.begin(), nums.end());
 
-int i = 0;
-int j = nums.size()-1;
-
-while(i<=j){
-    int mid = i + (j-i)/2;
-
-    if(nums[mid] == target) return true;
-
-    if(target < nums[mid])
-        j = mid - 1;
-    else
-        i = mid + 1;
-}
-
-return false;
+return s.count(target);
     }
 };
