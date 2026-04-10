b6341e7fe0d755d2d28d9be7fccd58daf39978a7
Arjun Divraniya <arjun.divraniya.cg@gmail.com>
2026-04-10 10:55:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
-void reversearr(vector<int>& nums,int s ,int e){
-    while(s<e){
-swap(nums[s] ,nums[e]);
-s++;
-e--;
-    }
-}
-
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k%n;
-        if(nums.size()- 1 <1 || k == 0)return;
-        reversearr(nums , 0 ,nums.size()-1);
-        reversearr(nums , 0 , k-1);
-       reversearr(nums , k ,nums.size()-1);
-       
+        k= k%n;
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++) {
+            ans[(i + k) % n] = nums[i];
+        }
+        nums = ans;
     }
 };
