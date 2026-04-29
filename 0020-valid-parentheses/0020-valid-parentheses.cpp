bbd1a4802f819b9fe5a76006086eda0d02cb66d6
Arjun Divraniya <arjun.divraniya.cg@gmail.com>
2026-04-29 10:03:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for (char c : s) {
-            if (c == '(' || c == '{' || c == '[') {
-                st.push(c);
-            } else {
-                if (st.empty()) {
-                    return false;
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.size()==0) return false;
+                if((st.top()=='{' && s[i]=='}') || 
+                    (st.top()=='[' && s[i]==']') ||
+                    (st.top()=='(' && s[i]==')')
+                ){
+                    st.pop();
                 }
-
-                if (c == ')' && st.top() != '(') {
-                    return false;
+                else{
+                     return false;
                 }
-                if (c == ']' && st.top() != '[') {
-                    return false;
-                }
-                if (c == '}' && st.top() != '{') {
-                    return false;
-                }
-                st.pop();
             }
         }
-            return st.empty();
-        }
-    };
+        return st.size()==0;
+    }
+};
