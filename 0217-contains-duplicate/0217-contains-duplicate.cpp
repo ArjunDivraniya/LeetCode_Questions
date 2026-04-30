09c3531cf19e7bdefeb8c5d5ef2c7f512e75d07a
Arjun Divraniya <arjun.divraniya.cg@gmail.com>
2026-05-01 00:05:35 +0530

    Time: 23 ms (88.01%), Space: 71 MB (89.15%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,49 +1,14 @@
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         for(int i=0;i<nums.size();i++){
-//             for(int j=i+1;j<nums.size();j++){
-//                 if(nums[i] == nums[j]){
-//                     return true;
-//                     break;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         sort(nums.begin(),nums.end());
-//         for(int i=0;i<nums.size()-1;i++){
-           
-//                 if(nums[i] == nums[i+1]){
-//                     return true;
-//                     break;
-//                 }
-//             }
-//         return false;
-//     }
-// };
-#include <unordered_set>
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-       unordered_set<int> ans;
-        for(int i=0;i<nums.size();i++){
-           
-                if(ans.count(nums[i])){
-                    return true;
-                    break;
-                }else{
-                    ans.insert(nums[i]);
-                }
+        sort(nums.begin(), nums.end());
+        int i =1;
+        while(i<nums.size()){
+            if(nums[i] == nums[i-1]){
+                return true;
             }
+            i++;
+        }
         return false;
     }
 };
-
-
